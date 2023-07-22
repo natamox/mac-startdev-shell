64f355dfd6ad218998b0e90c48c952855f00d0b2
natamox <natamoxc@gmail.com>
2023-07-22 22:03:51 +0800

    feat: fix bug

--- starter.sh
+++ starter.sh
@@ -1,24 +1,3 @@
-# 如果出现错误，立即终止脚本
-set -e
-
-PROXY_PORT=7890
-
-echo "建议开启终端代理，并根据代理的端口修改，默认 7890"
-
-read -p "是否有代理，请输入选择 [y/n]: " is_proxy
-
-if [ "$is_proxy" == "y" || -z "$is_proxy" ]; then
-  read -p "输入代理端口号: " proxy_port
-  if expr $proxy_port : '\([0-9][0-9]*\)' >/dev/null; then
-    PROXY_PORT=$proxy_port
-  fi
-  start_proxy
-  # for test
-  curl -I https://www.google.com
-fi
-
-start_download
-
 start_proxy() {
   export http_proxy="http://127.0.0.1:$PROXY_PORT"
   export https_proxy="http://127.0.0.1:$PROXY_PORT"
@@ -58,3 +37,27 @@ start_download() {
   echo "开始下载 yarn"
   brew install yarn
 }
+
+
+# 如果出现错误，立即终止脚本
+set -e
+
+PROXY_PORT=7890
+
+echo "建议开启终端代理，并根据代理的端口修改，默认 7890"
+
+read -p "是否有代理，请输入选择 [y/n]: " is_proxy
+
+if [ "$is_proxy" == 'y' ] || [ -z "$is_proxy" ]; then
+  read -p "输入代理端口号: " proxy_port
+  if expr $proxy_port : '\([0-9][0-9]*\)' >/dev/null; then
+    PROXY_PORT=$proxy_port
+  fi
+  start_proxy
+  # for test
+  curl -I https://www.google.com
+fi
+
+# start_download
+
+
